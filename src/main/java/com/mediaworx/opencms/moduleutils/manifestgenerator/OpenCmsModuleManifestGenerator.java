9be1f756ee763b197eac01abda19b957a00909ae
Andreas Karajannis <karajannis@mediaworx.com>
2014-01-30 17:16:45 +0100

    Add licensing information / copyright

--- src/main/java/com/mediaworx/opencms/moduleutils/manifestgenerator/OpenCmsModuleManifestGenerator.java
+++ src/main/java/com/mediaworx/opencms/moduleutils/manifestgenerator/OpenCmsModuleManifestGenerator.java
@@ -1,3 +1,24 @@
+/**
+ * Copyright (c) 2014 mediaworx berlin AG (http://mediaworx.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * For further information about mediaworx berlin AG, please see the
+ * company website: http://mediaworx.com
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ * If not, see <http://www.gnu.org/licenses/>
+ */
+
 package com.mediaworx.opencms.moduleutils.manifestgenerator;
 
 import com.mediaworx.opencms.moduleutils.manifestgenerator.exceptions.OpenCmsMetaXmlFileWriteException;
