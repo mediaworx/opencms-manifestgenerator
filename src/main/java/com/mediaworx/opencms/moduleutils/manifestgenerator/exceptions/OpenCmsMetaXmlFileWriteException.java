69458f9335bf64857c3f491e459eef4d353c0929
Kai Widmann <widmann@mediaworx.com>
2014-06-16 12:18:00 +0200

    Extended Javadoc, parameter rename (no functional change), updated pom version after documentation change

--- src/main/java/com/mediaworx/opencms/moduleutils/manifestgenerator/exceptions/OpenCmsMetaXmlFileWriteException.java
+++ src/main/java/com/mediaworx/opencms/moduleutils/manifestgenerator/exceptions/OpenCmsMetaXmlFileWriteException.java
@@ -1,8 +1,16 @@
 package com.mediaworx.opencms.moduleutils.manifestgenerator.exceptions;
 
+/**
+ * Exception used when there's an error writing the manifest file to the target
+ */
 public class OpenCmsMetaXmlFileWriteException extends Exception {
 
-	public OpenCmsMetaXmlFileWriteException(String s, Throwable throwable) {
-		super(s, throwable);
+	/**
+	 * Creates a new OpenCmsMetaXmlFileWriteException with a throwable that was causing the exception
+	 * @param s description of the error
+	 * @param cause the root cause of the Exception
+	 */
+	public OpenCmsMetaXmlFileWriteException(String s, Throwable cause) {
+		super(s, cause);
 	}
 }
--- src/main/java/com/mediaworx/opencms/moduleutils/manifestgenerator/exceptions/OpenCmsMetaXmlParseException.java
+++ src/main/java/com/mediaworx/opencms/moduleutils/manifestgenerator/exceptions/OpenCmsMetaXmlParseException.java
@@ -1,8 +1,16 @@
 package com.mediaworx.opencms.moduleutils.manifestgenerator.exceptions;
 
+/**
+ * Exception used when there's an error parsing xml for the manifest
+ */
 public class OpenCmsMetaXmlParseException extends Exception {
 
-	public OpenCmsMetaXmlParseException(String s, Throwable throwable) {
-		super(s, throwable);
+	/**
+	 * Creates a new OpenCmsMetaXmlParseException with a throwable that was causing the exception
+	 * @param s description of the error
+	 * @param cause the root cause of the Exception
+	 */
+	public OpenCmsMetaXmlParseException(String s, Throwable cause) {
+		super(s, cause);
 	}
 }
